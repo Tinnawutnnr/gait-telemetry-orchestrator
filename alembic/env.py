cb2634d8835c8dac4e5301facab0b40e145d659b
Tinnawutnnr <Tinnawut.nnr@gmail.com>
2026-03-06 13:59:16 +0700

    feat: implement identity and gait analysis schema with alembic migrations

--- alembic/env.py
+++ alembic/env.py
@@ -14,9 +14,8 @@ if config.config_file_name is not None:
 config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 
 # Add SQLAlchemy models' MetaData here for 'autogenerate' support.
-# from app.models import Base
-# target_metadata = Base.metadata
-target_metadata = None
+from app.models import Base
+target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
